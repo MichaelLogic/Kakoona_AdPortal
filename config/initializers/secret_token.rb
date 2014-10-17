596edc10a3c07b8b31a05a742bafbf5825800287
Michael Logic <ml@michaellogic.com>
2014-10-17 10:41:59 -0500

    AdPortal Algorithm First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KakoonaAdportal::Application.config.secret_key_base = '1ae4d5b01971a279da5769385f1b3cd42cd71fcccbb3a90da314deca47d6e5570a6c4480590300f2ab77aa0ab3808975928d17dde905fc2e639755d13af273e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KakoonaAdportal::Application.config.session_store :cookie_store, key: '_Kakoona_AdPortal_session'
