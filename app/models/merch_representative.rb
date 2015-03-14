64ff2c38f142c6a09b668bf641bff426e769d13d
Michael Logic <ml@michaellogic.com>
2015-03-13 19:38:32 -0500

    Correct Merch Representative Model

--- app/models/merch_representative.rb
+++ app/models/merch_representative.rb
@@ -1,6 +1,4 @@
-class MerchRepresentative < ActiveRecord::Base
-  attr_accessible :screen_name, :first_name, :middle_name, :last_name, :phone, :team_role, :rep_about
-  has_one :precious 
+class MerchRepresentative < ActiveRecord::Base 
   belongs_to :merchant
   validates :merchant_id, presence: true
 end
